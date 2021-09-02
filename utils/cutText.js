274737d8f33b5eb723dd672a747a0dbbda8a1d8b
wojbiel833 <wojbiel833@gmail.com>
2021-09-02 15:09:22 +0200

    Fix some tests in cutText function

--- utils/cutText.js
+++ utils/cutText.js
@@ -1,7 +1,8 @@
 module.exports = (content, maxLength) => {
   if (typeof content !== "string") return "Error";
-  if (typeof maxLength !== Number) return "Error";
+  if (typeof maxLength !== "number") return "Error";
   if (content.length < 1) return "Error";
+  if (maxLength <= 0) return "Error";
   console.log(content.length);
   if (content.length <= maxLength) return content;
   return content.substr(0, content.lastIndexOf(" ", maxLength)) + "...";
